6f2b1966eb93c4d17057670211a537f43329608b
Greg Dardis <dardis.greg@gmail.com>
2017-07-31 12:45:57 -0600

    implemented open method

--- src/Percolation.java
+++ src/Percolation.java
@@ -18,36 +18,79 @@ public class Percolation {
 		UF = new WeightedQuickUnionUF((gridSize * gridSize) + 2);
 	}
 	
+	/**
+	 * Converts 2D coordinates from the 2D array (1 indexed) to a 1D object index in the array.
+	 * 
+	 * @return 1D object index corresponding to the given spot in the 2D array
+	 */
 	private int rowAndColumnTo1D(int row, int col) {
 	    return row + (col - 1) * gridSize;
 	}
 	
+	/**
+	 * Opens the square with the given row and column in the 2D array, and connects it to all 
+	 * open squares that are touching it.
+	 */
 	public void open(int row, int col) {
-//	   if (open[row][col]) {
-//	       return;
-//	   } else {
-//	       open[row][col] = true;
-//	       // connect to neighbours
-//	       numberOpen++;
-//	   }
+	    if (!rowAndColumnInBounds(row, col)) {
+	        throw new IndexOutOfBoundsException();
+	    }
 	    
+	    open[row - 1][col - 1] = true;
+	    numberOpen++;
+	    
+	    if (row == 1) {
+	        UF.union(rowAndColumnTo1D(row, col), virtualTop);
+	    } else if (row == gridSize) {
+	        UF.union(rowAndColumnTo1D(row, col), virtualBottom);
+	    }
+	    
+	    if (row > 1 && isOpen(row - 1, col)) {
+	        UF.union(rowAndColumnTo1D(row, col), rowAndColumnTo1D(row - 1, col));
+	    }
+	    
+	    if (row < gridSize && isOpen(row + 1, col)) {
+	        UF.union(rowAndColumnTo1D(row, col), rowAndColumnTo1D(row + 1, col));
+	    }
+	    
+	    if (col > 1 && isOpen(row, col - 1)) {
+	        UF.union(rowAndColumnTo1D(row, col), rowAndColumnTo1D(row, col - 1));
+	    }
+	    
+	    if (col < gridSize && isOpen(row, col + 1)) {
+            UF.union(rowAndColumnTo1D(row, col), rowAndColumnTo1D(row, col + 1));
+        }
 	}
 	
+	/**
+	 * @return True if the square corresponding to the given row/column is open, false otherwise.
+	 */
 	public boolean isOpen(int row, int col) {
-	    return open[row][col];
+	    return open[row - 1][col - 1];
 	}
 	
+	/**
+	 * @return  True if the square for the given row/column is connected to the top row by open squares, false otherwise.
+	 */
 	public boolean isFull(int row, int col) {
-	    if (row > 0 && row <= gridSize && col > 0 && col <= gridSize) {
+	    if (rowAndColumnInBounds(row, col)) {
 	        return UF.connected(virtualTop, rowAndColumnTo1D(row, col));
 	    } else {
 	        throw new IndexOutOfBoundsException();
 	    }
 	}
 	
-	public int numberOfOpenSites() {
-//	    return numberOpen;
+	private boolean rowAndColumnInBounds(int row, int col) {
+	    return row > 0 && row <= gridSize && col > 0 && col <= gridSize;
 	}
+	
+	/**
+	 * @return  The number of sites that have been opened.
+	 */
+	public int numberOfOpenSites() {
+	    return numberOpen;
+	}
+	
 	public boolean percolates() {
 	    // does the system percolate?
 	}
